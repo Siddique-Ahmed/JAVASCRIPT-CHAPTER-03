004e5fadf3a0b557cf58ee0874d0109661dea84c
Siddique-Ahmed <itssiddique786@gmail.com>
2024-03-09 04:46:32 -0800

    chapter 03

---
+++ script.js
@@ -0,0 +1,34 @@
+var Age = "I am 23 Years old"
+
+alert(Age);
+
+var birthDay = 2001;
+
+document.write("My Birth year is " + birthDay);
+document.write("<br>")
+document.write("Data Type of my declared variable is Number");
+document.write("<br>")
+
+var userVisit = "You are visited 45 Times";
+
+alert(userVisit);
+
+var userName = prompt("Name");
+
+document.write("<br>");
+document.write("Name :" + userName);
+
+var userEmail = prompt("Enter your Email");
+
+document.write("<br>");
+document.write( "Email : " + userEmail);
+
+var userOrder = prompt("What you want");
+
+document.write("<br>");
+document.write("Order :" + userOrder);
+
+var userAddress = prompt("Enter your Address");
+
+document.write("<br>");
+document.write("Address : " + userAddress);
